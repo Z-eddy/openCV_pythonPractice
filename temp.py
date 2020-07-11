6854e6dcdc92871a083bc2551fb4e0080de64ae4
Z-eddy <zrendless@foxmail.com>
2020-07-11 11:31:54 +0800

    转换图片、保存图片
    cvtColor
    imwrite

--- temp.py
+++ temp.py
@@ -1,6 +1,20 @@
 import cv2
-matSrc = cv2.imread("images/dog.jpg")
-cv2.namedWindow("picture", cv2.WINDOW_AUTOSIZE)
-cv2.imshow("picture", matSrc)
+# 读取并显示原始图片
+src = cv2.imread("images/dog.jpg")
+winTitle = "dogPic"
+cv2.namedWindow(winTitle, cv2.WINDOW_AUTOSIZE)
+cv2.imshow(winTitle, src)
 cv2.waitKey(0)
+# 转换并显示灰度图
+gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+cv2.imshow(winTitle, gray)
+cv2.waitKey(0)
+# 保存转换的灰度图
+filePath = "images/grayDog.jpg"
+ok = cv2.imwrite(filePath, gray)
+if ok:
+    print("图片生成完毕")
+else:
+    print("图片生成失败")
+
 cv2.destroyAllWindows()
