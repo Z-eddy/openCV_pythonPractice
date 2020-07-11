a1d6c54de456965d1355b2575209d29ca6f44bfe
Z-eddy <zrendless@foxmail.com>
2020-07-11 10:27:42 +0800

    imshow不能做交互,放入namedWindow后可以

--- temp.py
+++ temp.py
@@ -1,5 +1,6 @@
 import cv2
-src = cv2.imread("./images/dog.jpg")
-cv2.imshow("picture", src)
+matSrc = cv2.imread("images/dog.jpg")
+cv2.namedWindow("picture", cv2.WINDOW_AUTOSIZE)
+cv2.imshow("picture", matSrc)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
