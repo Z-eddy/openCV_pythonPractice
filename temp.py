9b61a36e4bf53f38524b46149d5b907e1a049e5f
Z-eddy <zrendless@foxmail.com>
2020-07-11 16:42:37 +0800

    图片像素处理,费事2.8秒,可怕

--- temp.py
+++ temp.py
@@ -1,29 +1,20 @@
 import cv2 as cv
-import numpy as np
-src = cv.imread("images/dog.jpg")
+import time
+fileName = "images/dog.jpg"
+src = cv.imread(fileName)
 winTitle = "dogPic"
 cv.namedWindow(winTitle, cv.WINDOW_AUTOSIZE)
-# clone图片 原图
-m1 = np.copy(src)
-cv.imshow(winTitle, m1)
+cv.imshow(winTitle, src)
 cv.waitKey(0)
-# 指针赋值 带黄色方块图
-m2 = src
-# w:100到200 h:200:300 通道(0b,1g,2r):1、2 的元素改色为255
-src[100:200, 200:300, 1:3] = 255
-cv.imshow(winTitle, m2)
-cv.waitKey(0)
-# 建立数组 黑色图
-m3 = np.zeros(src.shape, src.dtype)
-cv.imshow(winTitle, m3)
-cv.waitKey(0)
-# 建立数组 指定颜色 灰色图
-m4 = np.zeros([512, 512], np.uint8)
-m4[:, :] = 127  # 所有通道都是127
-cv.imshow(winTitle, m4)
-cv.waitKey(0)
-# 建立数组 指定颜色 蓝色
-m5 = np.zeros(shape=[512, 512, 3], dtype=np.uint8) #512*512,3通道,数据类型为uint8
-m5[:, :, 0] = 255
-cv.imshow(winTitle, m5)
+row, col, chn = src.shape
+start = time.perf_counter_ns()
+for i in range(row):
+    for j in range(col):
+        b, g, r = src[i, j]
+        b = 255 - b
+        g = 255 - g
+        r = 255 - r
+        src[i, j] = [b, g, r]
+print(time.perf_counter_ns() - start)
+cv.imshow(winTitle, src)
 cv.waitKey(0)
