d51396575c94fb80aa37ceba2f35dcf081972589
Z-eddy <576172400@qq.com>
2020-07-14 23:02:09 +0800

    通道分离、合并

--- temp.py
+++ temp.py
@@ -1,27 +1,16 @@
 import cv2 as cv
 import numpy as np
 
-# 黄色方块
-yelloBlock=np.zeros(shape=[400,400,3],dtype=np.uint8)
-yelloBlock[100:200,100:200,1:3]=255
-cv.imshow("yelloBlock",yelloBlock)
-# 红色方块
-redBlock=np.zeros(shape=[400,400,3],dtype=np.uint8)
-redBlock[150:250,150:250,2]=255
-cv.imshow("redBlock",redBlock)
-# and
-andSrc=cv.bitwise_and(yelloBlock,redBlock)
-cv.imshow("and",andSrc)
-# or
-orSrc=cv.bitwise_or(yelloBlock,redBlock)
-cv.imshow("or",orSrc)
-# xor
-xorSrc=cv.bitwise_xor(yelloBlock,redBlock)
-cv.imshow("xor",xorSrc)
-# not
-notSrc=cv.imread("./images/dog.jpg")
-cv.imshow("originalDog",notSrc)
-notSrc=cv.bitwise_not(notSrc)
-cv.imshow("not",notSrc)
-
+src = cv.imread("images/dog.jpg")
+winTitle = "dogPic"
+cv.namedWindow(winTitle, cv.WINDOW_AUTOSIZE)
+cv.imshow(winTitle, src)  # 原图显示
 cv.waitKey(0)
+
+# 分别打印BGR通道置零时的图片
+for i in range(3):
+    mv = cv.split(src)
+    mv[i][:, :] = 0
+    rst = cv.merge(mv)
+    cv.imshow(winTitle, rst)
+    cv.waitKey(0)
