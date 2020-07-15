91470325f1febf45bbad5e220eb0691584981c75
Z-eddy <576172400@qq.com>
2020-07-16 00:02:00 +0800

    minMaxLoc,meanStdDev,np.where

--- temp.py
+++ temp.py
@@ -1,18 +1,25 @@
-import cv2 as cv
+import cv2.cv2 as cv
 import numpy as np
-src = cv.imread("G:/Practice/openCV/images/TinyGreen.PNG")
-res = cv.cvtColor(src, cv.COLOR_BGR2YUV)
-cv.imshow("YUV", res)  # YUV
-res = cv.cvtColor(src, cv.COLOR_BGR2YCrCb)
-cv.imshow("YCrCb", res)  # YCrCb
-res = cv.cvtColor(src, cv.COLOR_BGR2HSV)
-cv.imshow("HSV", res)  # HSV
 
-res = cv.inRange(res, (35, 43, 46), (77, 255, 255))  # 二值化
-cv.imshow("inRange", res)
+src = cv.imread("./images/dog.jpg")
+winTitle = "dogImg"
+cv.namedWindow(winTitle, cv.WINDOW_AUTOSIZE)
+cv.imshow(winTitle, src)
+cv.waitKey(0)
 
-res = cv.cvtColor(res, cv.COLOR_GRAY2BGR)  # 灰度图转换3通道
-res = cv.add(src, res)  # 抠图
-cv.imshow("final", res)
+# 求取平均值、方差
+means, stdDev = cv.meanStdDev(src)
+for i in range(len(means)):
+    print("平均值:%.2f,方差:%.2f" % (means[i], stdDev[i]))
 
+# 输出黑白色
+src1 = cv.split(src)[0]  # 蓝色通道的值
+cv.imshow(winTitle, src1)
+cv.waitKey(0)
+minV, maxV, minLoc, maxLoc = cv.minMaxLoc(src1)
+mean = src1.mean()
+print(minV, maxV, minLoc, maxLoc)
+src1[np.where(src1 >= mean)] = 255
+src1[np.where(src1 < mean)] = 0
+cv.imshow(winTitle, src1)
 cv.waitKey(0)
