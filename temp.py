7c9274e15ce5639ccf35728c3bfd80e55dac2754
Z-eddy <zrendless@foxmail.com>
2020-07-17 18:05:27 +0800

    flip翻转,0:x轴,1:y轴,-1:xy翻转

--- temp.py
+++ temp.py
@@ -1,21 +1,16 @@
 import cv2.cv2 as cv
 import numpy as np
 
-video = cv.VideoCapture(
-    r"D:\codeSofts\opencv\opencv\sources\samples\data\vtest.avi")
-width = video.get(cv.CAP_PROP_FRAME_WIDTH)
-height = video.get(cv.CAP_PROP_FRAME_HEIGHT)
-fps = video.get(cv.CAP_PROP_FPS)
+src=cv.imread("./images/dog.jpg")
+cv.imshow("original",src)
 
-# writer=cv.VideoWriter("videoDemo.mp4",cv.CAP_PROP_FOCUS,fps,(np.int(width),np.int(height)),True)
-writer = cv.VideoWriter("./videoDemo.mp4", cv.CAP_PROP_FOCUS,
-                        fps, (np.int(width), np.int(height)), True)
-while True:
-    ok, frame = video.read()
-    if ok:
-        writer.write(frame)
-    else:
-        break
+dst=cv.flip(src,0)
+cv.imshow("x",dst)
 
-video.release()
-writer.release()
+dst=cv.flip(src,1)
+cv.imshow("y",dst)
+
+dst=cv.flip(src,-1)
+cv.imshow("xy",dst)
+
+cv.waitKey(0)
