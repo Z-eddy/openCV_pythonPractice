164f93fe692b195b7598d3b0b56f28d62e266a32
Z-eddy <576172400@qq.com>
2020-07-18 00:11:29 +0800

    resize的4种放大缩小的插值方法

--- temp.py
+++ temp.py
@@ -1,16 +1,22 @@
 import cv2.cv2 as cv
 import numpy as np
 
-src=cv.imread("./images/dog.jpg")
+src=cv.imread("images/dog.jpg")
 cv.imshow("original",src)
 
-dst=cv.flip(src,0)
-cv.imshow("x",dst)
+w,h=src.shape[:2]
+size=(np.int(w*0.75),np.int(h*0.75))
 
-dst=cv.flip(src,1)
-cv.imshow("y",dst)
+dst=cv.resize(src,size,0,0,cv.INTER_NEAREST)
+cv.imshow("NEARST",dst)
 
-dst=cv.flip(src,-1)
-cv.imshow("xy",dst)
+dst=cv.resize(src,size,0,0,cv.INTER_LINEAR)
+cv.imshow("LINEAR",dst)
+
+dst=cv.resize(src,size,0,0,cv.INTER_CUBIC)
+cv.imshow("CUBIC",dst)
+
+dst=cv.resize(src,size,0,0,cv.INTER_LANCZOS4)
+cv.imshow("LANCZOS4",dst)
 
 cv.waitKey(0)
