feed1b85f7fb81119a291b9ab2ee9119fd9852db
Z-eddy <576172400@qq.com>
2020-07-18 22:43:34 +0800

    line绘制直线,rectangle绘制矩形,线宽-1会填充

--- temp.py
+++ temp.py
@@ -1,22 +1,8 @@
 import cv2.cv2 as cv
 import numpy as np
 
-src=cv.imread("images/dog.jpg")
-cv.imshow("original",src)
-
-w,h=src.shape[:2]
-size=(np.int(w*0.75),np.int(h*0.75))
-
-dst=cv.resize(src,size,0,0,cv.INTER_NEAREST)
-cv.imshow("NEARST",dst)
-
-dst=cv.resize(src,size,0,0,cv.INTER_LINEAR)
-cv.imshow("LINEAR",dst)
-
-dst=cv.resize(src,size,0,0,cv.INTER_CUBIC)
-cv.imshow("CUBIC",dst)
-
-dst=cv.resize(src,size,0,0,cv.INTER_LANCZOS4)
-cv.imshow("LANCZOS4",dst)
-
+dst=np.zeros((400,400,3),np.uint8)
+cv.line(dst,(100,100),(200,200),(255,0,0),1)
+cv.rectangle(dst,(200,200),(300,300),(0,0,255),-1)
+cv.imshow("test",dst)
 cv.waitKey(0)
