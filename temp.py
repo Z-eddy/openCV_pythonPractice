694c06273feff8808bd8e57f7632467111e55551
Z-eddy <576172400@qq.com>
2020-07-20 23:50:21 +0800

    calcHist计算直方图

--- temp.py
+++ temp.py
@@ -1,12 +1,14 @@
 import cv2.cv2 as cv
 import numpy as np
+from matplotlib import pyplot as plt
 
-src = cv.imread("G:/Practice/openCV/images/TinyGreen.PNG")
-hsv = cv.cvtColor(src, cv.COLOR_BGR2HSV)  # hsv
-mask = cv.inRange(hsv, (36, 43, 46), (99, 255, 255))  # 掩膜
-blueBg = np.zeros(src.shape, src.dtype)
-blueBg[:, :, 0] = 255
-dst = cv.bitwise_and(blueBg, blueBg, src, mask)
-cv.imshow("dst", dst)
+def image_hist(src):
+    color=("blue","green","red")
+    for key,value in enumerate(color):
+        hist=cv.calcHist([src],[key],None,[256],[0,256])
+        plt.plot(hist,value)
+    plt.show()
 
-cv.waitKey(0)
+src=cv.imread("images/dog.jpg")
+cv.imshow("original",src)
+image_hist(src)
