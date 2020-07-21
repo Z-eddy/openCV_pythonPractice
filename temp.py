2b336838a1d32b76772f6997a55873f4b095d2a9
Z-eddy <zrendless@foxmail.com>
2020-07-21 17:38:32 +0800

    compareHist比较直方图,相关性HISTCMP_CORREL,巴氏HISTCMP_BHATTACHARYYA两种方法较好

--- temp.py
+++ temp.py
@@ -1,13 +1,30 @@
 import cv2.cv2 as cv
 import numpy as np
 
-src=cv.imread("images/dog.jpg")
-cv.imshow("original",src)
+src0=cv.imread("D:/codeSofts/opencv/sources/samples/data/lena.jpg");
+src1=cv.imread("D:/codeSofts/opencv/sources/samples/data/lena_tmpl.jpg");
+src2=cv.imread("D:/codeSofts/opencv/sources/samples/data/aero1.jpg");
+src3=cv.imread("D:/codeSofts/opencv/sources/samples/data/aero3.jpg");
 
-chanls=cv.split(src)
-for i in range(3):
-    chanls[i]=cv.equalizeHist(chanls[i])
-dst=cv.merge(chanls)
-cv.imshow("equalizeHist",dst)
+src0=cv.cvtColor(src0,cv.COLOR_BGR2HSV)
+src1=cv.cvtColor(src1,cv.COLOR_BGR2HSV)
+src2=cv.cvtColor(src2,cv.COLOR_BGR2HSV)
+src3=cv.cvtColor(src3,cv.COLOR_BGR2HSV)
 
-cv.waitKey(0)
+src0=cv.calcHist([src0],[0,1],None,[60,64],[0,180,0,256])
+src1=cv.calcHist([src1],[0,1],None,[60,64],[0,180,0,256])
+src2=cv.calcHist([src2],[0,1],None,[60,64],[0,180,0,256])
+src3=cv.calcHist([src3],[0,1],None,[60,64],[0,180,0,256])
+
+cv.normalize(src0,src0,1,0,cv.NORM_MINMAX)
+cv.normalize(src1,src1,1,0,cv.NORM_MINMAX)
+cv.normalize(src2,src2,1,0,cv.NORM_MINMAX)
+cv.normalize(src3,src3,1,0,cv.NORM_MINMAX)
+
+v01=cv.compareHist(src0,src1,cv.HISTCMP_CORREL)
+v23=cv.compareHist(src2,src3,cv.HISTCMP_CORREL)
+print(v01,v23)
+
+v01=cv.compareHist(src0,src1,cv.HISTCMP_BHATTACHARYYA)
+v23=cv.compareHist(src2,src3,cv.HISTCMP_BHATTACHARYYA)
+print(v01,v23)
