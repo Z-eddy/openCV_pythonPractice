04cbea445b5b7aff09b698dc3599fb94792b0f69
Z-eddy <zrendless@foxmail.com>
2020-07-21 14:25:38 +0800

    equalizeHist直方图均衡化

--- temp.py
+++ temp.py
@@ -1,24 +1,13 @@
 import cv2.cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
-
-# 使用函数批量解析
-def image_hist(img):
-    color=("blue","green","red")
-    for key,value in enumerate(color):
-        hist=cv.calcHist([img],[key],None,[256],[0,256])
-        plt.plot(hist,value)
-    plt.show()
 
 src=cv.imread("images/dog.jpg")
-image_hist(src)
-'''
-# 手动解析
-bHist=cv.calcHist([src],[0],None,[256],[0,256])
-gHist=cv.calcHist([src],[1],None,[256],[0,256])
-rHist=cv.calcHist([src],[2],None,[256],[0,256])
-plt.plot(bHist,"blue")
-plt.plot(gHist,"green")
-plt.plot(rHist,"red")
-plt.show()
-'''
+cv.imshow("original",src)
+
+chanls=cv.split(src)
+for i in range(3):
+    chanls[i]=cv.equalizeHist(chanls[i])
+dst=cv.merge(chanls)
+cv.imshow("equalizeHist",dst)
+
+cv.waitKey(0)
