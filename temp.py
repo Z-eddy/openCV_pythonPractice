4ba675d41e99656a3c5d8e4cfc14c65015285951
Z-eddy <zrendless@foxmail.com>
2020-07-21 11:25:26 +0800

    calcHist手动解析、自动解析

--- temp.py
+++ temp.py
@@ -2,13 +2,23 @@ import cv2.cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-def image_hist(src):
+# 使用函数批量解析
+def image_hist(img):
     color=("blue","green","red")
     for key,value in enumerate(color):
-        hist=cv.calcHist([src],[key],None,[256],[0,256])
+        hist=cv.calcHist([img],[key],None,[256],[0,256])
         plt.plot(hist,value)
     plt.show()
 
 src=cv.imread("images/dog.jpg")
-cv.imshow("original",src)
 image_hist(src)
+'''
+# 手动解析
+bHist=cv.calcHist([src],[0],None,[256],[0,256])
+gHist=cv.calcHist([src],[1],None,[256],[0,256])
+rHist=cv.calcHist([src],[2],None,[256],[0,256])
+plt.plot(bHist,"blue")
+plt.plot(gHist,"green")
+plt.plot(rHist,"red")
+plt.show()
+'''
