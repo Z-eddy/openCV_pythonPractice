ee37d359194775c00557aa48937d44f764e819dc
Zr <zrendless@foxmail.com>
2020-07-22 16:34:15 +0800

    blur卷积模糊

--- temp.py
+++ temp.py
@@ -1,17 +1,10 @@
-import cv2.cv2 as cv
+﻿import cv2.cv2 as cv
 import numpy as np
-image=cv.imread(r"E:\practice\openCV\material\images\target.png")
-cv.imshow("original",image)
-theModel=cv.imread(r"E:\practice\openCV\material\images\sample.png")
-# 颜色空间转换
-image=cv.cvtColor(image,cv.COLOR_BGR2HSV)
-theModel=cv.cvtColor(theModel,cv.COLOR_BGR2HSV)
-# 计算model直方图,180/30,256/32
-roiHist=cv.calcHist([theModel],(0,1),None,(30,32),(0,180,0,256))
-# 归一化
-roiHist=cv.normalize(roiHist,roiHist,255,0,cv.NORM_MINMAX)
-# 计算直方图反向投影
-dst=cv.calcBackProject([image],(0,1),roiHist,(0,180,0,256),1)
-cv.imshow("result",dst)
+
+src=cv.imread("images/dog.jpg")
+cv.imshow("original",src)
+
+src=cv.blur(src,(3,3))
+cv.imshow("卷积后",src)
 
 cv.waitKey(0)
