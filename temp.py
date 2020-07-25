8309d538112de3fcdd277708ca0f09ec95578dd1
Zr <576172400@qq.com>
2020-07-26 00:12:17 +0800

    bilateralFilter高斯双边模糊

--- temp.py
+++ temp.py
@@ -1,11 +1,13 @@
 ﻿import cv2.cv2 as cv
 import numpy as np
 
-src=cv.imread("images/dog.jpg")
-src=cv.UMat(src)
+src= cv.imread("images/dog.jpg")
+# src = cv.UMat(src)
 cv.imshow("original",src)
-
-src=cv.fastNlMeansDenoisingColored(src,None)
-cv.imshow("mediaBlur",src)
-
+dst=cv.bilateralFilter(src,0,100,5)
+h,w=src.shape[:2] # 不支持UMat
+result=np.zeros((h,w*2,3),src.dtype)
+result[0:h,0:w,:]=src
+result[0:h,w:2*w,:]=dst
+cv.imshow("result",result)
 cv.waitKey(0)
