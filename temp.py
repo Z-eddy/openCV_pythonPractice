393a65a581fb672f79e57351749e79e7ac069769
Zr <576172400@qq.com>
2020-07-26 22:45:29 +0800

    integral图像积分

--- temp.py
+++ temp.py
@@ -1,13 +1,8 @@
 ﻿import cv2.cv2 as cv
 import numpy as np
 
-src= cv.imread("images/dog.jpg")
-# src = cv.UMat(src)
-cv.imshow("original",src)
-dst=cv.bilateralFilter(src,0,100,5)
-h,w=src.shape[:2] # 不支持UMat
-result=np.zeros((h,w*2,3),src.dtype)
-result[0:h,0:w,:]=src
-result[0:h,w:2*w,:]=dst
-cv.imshow("result",result)
-cv.waitKey(0)
+src=cv.imread("images/dog.jpg")
+integralDst=cv.integral(src) # 图像积分
+for i in range(3):
+    val=integralDst[15][15][i]
+    print(val)
