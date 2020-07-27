53765b977dffe4596c5155696505b492ca591360
Zr <zrendless@foxmail.com>
2020-07-27 08:58:47 +0800

    金字塔均值迁移滤波

--- temp.py
+++ temp.py
@@ -1,8 +1,7 @@
 ﻿import cv2.cv2 as cv
 import numpy as np
-
 src=cv.imread("images/dog.jpg")
-integralDst=cv.integral(src) # 图像积分
-for i in range(3):
-    val=integralDst[15][15][i]
-    print(val)
+src=cv.UMat(src)
+dst=cv.pyrMeanShiftFiltering(src,15,30)
+cv.imshow("result", dst)
+cv.waitKey(0)
