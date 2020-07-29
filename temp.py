fd457cb6a5e3c3f74dcc5aa9fbe7a34aa2c15593
Zr <576172400@qq.com>
2020-07-29 23:58:42 +0800

    filter2D自定义滤波器

--- temp.py
+++ temp.py
@@ -1,9 +1,27 @@
 ﻿import cv2.cv2 as cv
 import numpy as np
 
-src=cv.imread("E:/openCV/opencv_tutorial/data/images/example.png")
-src=cv.UMat(src)
+src=cv.imread("images/dog.jpg")
 cv.imshow("original",src)
-dst=cv.edgePreservingFilter(src,None,1,100,0.4)
-cv.imshow("edgePreservingFilter",dst)
+# 算子0
+kernel0 = np.ones([5, 5], np.float32)/25
+dst=cv.filter2D(src,-1,kernel0)
+cv.imshow("kernel0",dst)
+# 算子1
+kernel1=np.array([
+    [0,-1,0],
+    [-1,5,-1],
+    [0,-1,0]
+],np.float32)
+dst=cv.filter2D(src,-1,kernel1)
+cv.imshow("kernel1",dst)
+# 算子2
+kernel2=np.array([
+    [1,0],
+    [0,-1]
+],np.float32)
+dst=cv.filter2D(src,-1,kernel2)
+cv.convertScaleAbs(dst,dst)
+cv.imshow("kernel2",dst)
+
 cv.waitKey(0)
