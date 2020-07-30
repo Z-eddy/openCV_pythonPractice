91a5d61eeb10a139190831b00ae5a65db6dbd6de
Zr <576172400@qq.com>
2020-07-30 23:47:30 +0800

    Sobel算子,对x/y方向一阶求导,然后手动add即可

--- temp.py
+++ temp.py
@@ -2,26 +2,16 @@
 import numpy as np
 
 src=cv.imread("images/dog.jpg")
-cv.imshow("original",src)
-# 算子0
-kernel0 = np.ones([5, 5], np.float32)/25
-dst=cv.filter2D(src,-1,kernel0)
-cv.imshow("kernel0",dst)
-# 算子1
-kernel1=np.array([
-    [0,-1,0],
-    [-1,5,-1],
-    [0,-1,0]
-],np.float32)
-dst=cv.filter2D(src,-1,kernel1)
-cv.imshow("kernel1",dst)
-# 算子2
-kernel2=np.array([
-    [1,0],
-    [0,-1]
-],np.float32)
-dst=cv.filter2D(src,-1,kernel2)
-cv.convertScaleAbs(dst,dst)
-cv.imshow("kernel2",dst)
+src=cv.UMat(src)
+xDst=cv.Sobel(src,cv.CV_32F,1,0)
+xDst=cv.convertScaleAbs(xDst)
+cv.imshow("xDst",xDst)
+yDst = cv.Sobel(src,cv.CV_32F,0,1)
+yDst = cv.convertScaleAbs(yDst)
+cv.imshow("yDst",yDst)
+
+dst=cv.add(xDst,yDst,dtype=cv.CV_16S)
+dst=cv.convertScaleAbs(dst)
+cv.imshow("dst",dst)
 
 cv.waitKey(0)
