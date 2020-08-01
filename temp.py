d2d654e1f429bf6c71c84402bae7ca59942d44a6
Zr <576172400@qq.com>
2020-08-02 00:15:58 +0800

    拉普拉斯算子加入高斯卷积

--- temp.py
+++ temp.py
@@ -2,7 +2,12 @@
 import numpy as np
 
 src=cv.imread("images/dog.jpg")
-dst=cv.Laplacian(src,cv.CV_32F,delta=150)
+cv.imshow("original",src)
+if True:
+    dst=cv.GaussianBlur(src,(3,3),0)
+    dst=cv.Laplacian(dst,cv.CV_32F,delta=150)
+else:
+    dst=cv.Laplacian(src,cv.CV_32F,delta=150)
 dst=cv.convertScaleAbs(dst)
 cv.imshow("Laplacian",dst)
 
