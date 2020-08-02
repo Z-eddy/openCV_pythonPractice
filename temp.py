349aa9bd90d335e9470b5e32ad757a3b6a6abd21
Zr <576172400@qq.com>
2020-08-02 22:22:16 +0800

    图像锐化,filter2D在拉普拉斯算子上,图像权重像素加强的输出

--- temp.py
+++ temp.py
@@ -1,14 +1,14 @@
 ﻿import cv2.cv2 as cv
 import numpy as np
 
-src=cv.imread("images/dog.jpg")
-cv.imshow("original",src)
-if True:
-    dst=cv.GaussianBlur(src,(3,3),0)
-    dst=cv.Laplacian(dst,cv.CV_32F,delta=150)
-else:
-    dst=cv.Laplacian(src,cv.CV_32F,delta=150)
+src = cv.imread("images/dog.jpg")
+sharpenAlgorithm = np.array([
+        [0, -1, 0],
+        [-1, 4.5, -1],
+        [0, -1, 0]
+    ], dtype=np.float32)
+dst=cv.filter2D(src,cv.CV_32F,sharpenAlgorithm)
 dst=cv.convertScaleAbs(dst)
-cv.imshow("Laplacian",dst)
+cv.imshow("sharpen", dst)
 
 cv.waitKey(0)
