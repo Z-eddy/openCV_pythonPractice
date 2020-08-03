ab2b4502263102be96f6060cef767f6e689aef3d
赵睿 <zrendless@foxmail.com>
2020-08-03 22:40:16 +0800

    USM,unsharpen mask,锐化增强算法
    addWeighted权重叠加

--- temp.py
+++ temp.py
@@ -1,14 +1,10 @@
 ﻿import cv2.cv2 as cv
 import numpy as np
 
-src = cv.imread("images/dog.jpg")
-sharpenAlgorithm = np.array([
-        [0, -1, 0],
-        [-1, 4.5, -1],
-        [0, -1, 0]
-    ], dtype=np.float32)
-dst=cv.filter2D(src,cv.CV_32F,sharpenAlgorithm)
-dst=cv.convertScaleAbs(dst)
-cv.imshow("sharpen", dst)
+src=cv.imread("images/dog.jpg")
+cv.imshow("original",src)
+gaussian=cv.GaussianBlur(src,(3,3),5)
+dst = cv.addWeighted(src, 1.5, gaussian, -0.5, 0)
+cv.imshow("USM",dst)
 
 cv.waitKey(0)
