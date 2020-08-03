640a5009295fe47a0a528c5562039dc6db54d2d5
赵睿 <zrendless@foxmail.com>
2020-08-03 23:22:26 +0800

    Canny边缘检测算法

--- temp.py
+++ temp.py
@@ -1,10 +1,7 @@
 ﻿import cv2.cv2 as cv
 import numpy as np
-
 src=cv.imread("images/dog.jpg")
-cv.imshow("original",src)
-gaussian=cv.GaussianBlur(src,(3,3),5)
-dst = cv.addWeighted(src, 1.5, gaussian, -0.5, 0)
-cv.imshow("USM",dst)
+dst=cv.Canny(src,100,300)
+cv.imshow("Canny", dst)
 
 cv.waitKey(0)
