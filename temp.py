52371e284af89ac8038abe132183758c624f9280
赵睿 <zrendless@foxmail.com>
2020-08-04 22:28:22 +0800

    拉普拉斯金字塔,叠加expand部分还原不清晰的图

--- temp.py
+++ temp.py
@@ -1,9 +1,14 @@
 ﻿import cv2.cv2 as cv
 import numpy as np
+
 src=cv.imread("images/dog.jpg")
-pyrDownDst=cv.pyrDown(src)
-cv.imshow("pyrDown",pyrDownDst)
-pyrUpDst=cv.pyrUp(pyrDownDst)
-cv.imshow("pyrUp",pyrUpDst)
+cv.imshow("original",src)
+temp0=cv.pyrDown(src) # 先reduce
+h,w=src.shape[:2]
+temp1=cv.pyrUp(temp0,dstsize=(w,h)) # 然后expand
+laplacian=cv.subtract(src,temp1) #计算拉普拉斯金字塔
+cv.imshow("laplacian",laplacian)
+dst=cv.add(temp1,laplacian) # 拉普拉斯+expand还原模糊图像
+cv.imshow("revert",dst)
 
 cv.waitKey(0)
