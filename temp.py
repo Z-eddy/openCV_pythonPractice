8ab0eeb9cca05afbb08031ffbe0f1c6b5d3517ee
赵睿 <zrendless@foxmail.com>
2020-08-06 16:00:37 +0800

    简单遍历进行二值图像分割

--- temp.py
+++ temp.py
@@ -1,20 +1,17 @@
 ﻿import cv2.cv2 as cv
 import numpy as np
+src=cv.imread("images/dog.jpg")
+src=cv.cvtColor(src,cv.COLOR_BGR2GRAY)
+t=127
+row,col=src.shape[:2]
+for i in range(row):
+    for j in range(col):
+        val= src[i, j]
+        if(val>t):
+            src[i, j]=255
+        else :
+            src[i, j]=0
 
-src=cv.imread("E:/practice/openCV/material/images/llk.jpg")
-tpl=cv.imread("E:/practice/openCV/material/images/llk_tpl.png")
-dst=cv.matchTemplate(src,tpl,cv.TM_CCORR_NORMED)
-cv.imshow("dst",dst)
-
-loc = np.where(dst > 0.95) # 获得坐标的索引
-print(loc,'\n\n') # 打印两个array
-print(loc[::],'\n\n') # 打印两个array
-print(loc[::-1],'\n\n') # 倒着打印两个array
-print(*loc[::-1],'\n\n') # 打印两个array转化的列表
-print(zip(*loc[::-1]),'\n\n') # 打印两个array转换的列表,然后组合成的Point
-h,w=tpl.shape[:2]
-for p in zip(*loc[::-1]):
-    cv.rectangle(src,p,(p[0]+w,p[1]+h),(0,0,255),1,8,0)
-cv.imshow("rectangle",src)
+cv.imshow("dst",src)
 
 cv.waitKey(0)
