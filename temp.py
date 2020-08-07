e1123b959586747ebef429723166f46820275365
赵睿 <zrendless@foxmail.com>
2020-08-08 00:17:48 +0800

    OTSU自动阈值分割

--- temp.py
+++ temp.py
@@ -1,8 +1,8 @@
 ﻿import cv2.cv2 as cv
 import numpy as np
-
-src= cv.imread("C:/Users/Zr/Desktop/temp/test.png")
-returnVal,dst=cv.threshold(src,127,255,cv.THRESH_BINARY) # python会有2个返回值,第一个是阈值大小
-cv.imshow("BINARY",dst)
+src= cv.imread("images/dog.jpg")
+src=cv.cvtColor(src,cv.COLOR_BGR2GRAY)
+dst=cv.threshold(src,0,255,cv.THRESH_BINARY|cv.THRESH_OTSU)
+cv.imshow("dst", dst)
 
 cv.waitKey(0)
