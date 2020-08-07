1f9a4e41c99e4770989a75dfc121e01d850ddb14
赵睿 <zrendless@foxmail.com>
2020-08-08 00:41:44 +0800

    thresh_triangle二值化,自动阈值

--- temp.py
+++ temp.py
@@ -1,8 +1,8 @@
 ﻿import cv2.cv2 as cv
 import numpy as np
-src= cv.imread("images/dog.jpg")
-src=cv.cvtColor(src,cv.COLOR_BGR2GRAY)
-dst=cv.threshold(src,0,255,cv.THRESH_BINARY|cv.THRESH_OTSU)
-cv.imshow("dst", dst)
+src = cv.imread("images/dog.jpg")
+src = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+val, dst = cv.threshold(src, 0, 255, cv.THRESH_BINARY | cv.THRESH_TRIANGLE)
+cv.imshow("threshold_triangle", dst)
 
 cv.waitKey(0)
