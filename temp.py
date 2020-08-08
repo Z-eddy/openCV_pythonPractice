9e110165f6b0d0b9124b253ea57b22039cd0215f
赵睿 <zrendless@foxmail.com>
2020-08-09 02:07:59 +0800

    adaptiveThreshold自适应阈值算法,适合光照不均匀的图片阈值分割

--- temp.py
+++ temp.py
@@ -1,8 +1,8 @@
 ﻿import cv2.cv2 as cv
 import numpy as np
-src = cv.imread("images/dog.jpg")
-src = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-val, dst = cv.threshold(src, 0, 255, cv.THRESH_BINARY | cv.THRESH_TRIANGLE)
-cv.imshow("threshold_triangle", dst)
+src=cv.imread("images/dog.jpg")
+src=cv.cvtColor(src,cv.COLOR_BGR2GRAY)
+dst=cv.adaptiveThreshold(src,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,25,10)
+cv.imshow("adaptiveThreshhold",dst)
 
 cv.waitKey(0)
