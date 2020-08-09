095c429ce4dad85cd6a0730a2902606dab4e802d
赵睿 <zrendless@foxmail.com>
2020-08-09 23:57:04 +0800

    图像去噪后二值化

--- temp.py
+++ temp.py
@@ -1,8 +1,10 @@
 ﻿import cv2.cv2 as cv
 import numpy as np
-src=cv.imread("images/dog.jpg")
+src=cv.imread("E:/practice/openCV/material/images/coins.jpg")
+# src=cv.blur(src,(3,3))
+src=cv.pyrMeanShiftFiltering(src,10,100)
 src=cv.cvtColor(src,cv.COLOR_BGR2GRAY)
-dst=cv.adaptiveThreshold(src,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,25,10)
-cv.imshow("adaptiveThreshhold",dst)
+val,dst= cv.threshold(src,0,255,cv.THRESH_BINARY|cv.THRESH_OTSU)
+cv.imshow("dst", dst)
 
 cv.waitKey(0)
