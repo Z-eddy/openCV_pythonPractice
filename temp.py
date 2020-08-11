b2cac8048f73e4e3157f10221e72b30b24c3b2cc
赵睿 <zrendless@foxmail.com>
2020-08-12 01:02:41 +0800

    connectedComponentsWithStats联通组件统计信息

--- temp.py
+++ temp.py
@@ -1,25 +1,19 @@
-﻿import cv2.cv2  as cv
+﻿import cv2.cv2 as cv
 import numpy as np
-src=cv.imread("E:/practice/openCV/material/images/rice.png") # 获得原图
-cv.imshow("original",src)
-src=cv.GaussianBlur(src,(3,3),0) # 高斯滤波
-gray=cv.cvtColor(src,cv.COLOR_BGR2GRAY) # 灰度图转换
-val,threshold=cv.threshold(gray,0,255,cv.THRESH_BINARY|cv.THRESH_OTSU) # 阈值分割
-num,labs=cv.connectedComponents(threshold) # 寻找到的数量,分布图
-colors=[]
-for i in range(num):
-    b=np.random.randint(20,255)
-    g=np.random.randint(20,255)
-    r=np.random.randint(20,255)
-    colors.append((b,g,r))
-colors[0]=(0,0,0)
+src = cv.imread("E:/practice/openCV/material/images/rice.png")
+cv.imshow("origianl",src)
+gaussian = cv.GaussianBlur(src, (3, 3), 0)
+gray = cv.cvtColor(gaussian, cv.COLOR_BGR2GRAY)
+n,thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+nums,labels,status,centers=cv.connectedComponentsWithStats(thresh)
+dst=src.copy()
+for i in range(1, nums, 1):
+    cx,cy=centers[i]
+    cv.circle(dst,(np.int32(cx),np.int32(cy)),2,(0,255,0),-1) # 绘制圆心
+    x,y,w,h,area=status[i]
+    cv.rectangle(dst,(x,y),(x+w,y+h),(0,255,0))
+    cv.putText(dst,"index:"+str(i),(x,y),cv.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),1)
 
-dst=np.zeros(src.shape,src.dtype)
-h,w=dst.shape[:2] # 如果是gray的图则无需[:2],因为没有第三个通道参数
-for i in range(h):
-    for j in range(w):
-        dst[i,j]=colors[labs[i,j]]
-cv.putText(dst, "find nums:"+str(num-1), (20, 50), cv.FONT_ITALIC, 1.0, (0, 0, 255))
 cv.imshow("dst",dst)
 
 cv.waitKey(0)
