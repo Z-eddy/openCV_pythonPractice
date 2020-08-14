c5bdd911ae25261be9445826095f681323d77c1c
赵睿 <zrendless@foxmail.com>
2020-08-15 00:32:05 +0800

    findContours发现轮廓,drawContours绘制轮廓

--- temp.py
+++ temp.py
@@ -1,19 +1,12 @@
 ﻿import cv2.cv2 as cv
 import numpy as np
-src = cv.imread("E:/practice/openCV/material/images/rice.png")
-cv.imshow("origianl",src)
-gaussian = cv.GaussianBlur(src, (3, 3), 0)
-gray = cv.cvtColor(gaussian, cv.COLOR_BGR2GRAY)
-n,thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
-nums,labels,status,centers=cv.connectedComponentsWithStats(thresh)
-dst=src.copy()
-for i in range(1, nums, 1):
-    cx,cy=centers[i]
-    cv.circle(dst,(np.int32(cx),np.int32(cy)),2,(0,255,0),-1) # 绘制圆心
-    x,y,w,h,area=status[i]
-    cv.rectangle(dst,(x,y),(x+w,y+h),(0,255,0))
-    cv.putText(dst,"index:"+str(i),(x,y),cv.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),1)
 
-cv.imshow("dst",dst)
+src=cv.imread("images/dog.jpg")
+canny=cv.Canny(src,100,200)
+contours,hierarchy=cv.findContours(canny,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+for i in range(len(contours)):
+    cv.drawContours(src,contours, i,(0,0,255))
+
+cv.imshow("dst",src)
 
 cv.waitKey(0)
