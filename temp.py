676409e379153509326a937bf8d05d5f8e502e17
赵睿 <zrendless@foxmail.com>
2020-08-16 23:50:57 +0800

    boundingRect最大矩形轮廓,minAreaRect最小矩形轮廓发现

--- temp.py
+++ temp.py
@@ -1,12 +1,16 @@
 ﻿import cv2.cv2 as cv
 import numpy as np
 
-src=cv.imread("images/dog.jpg")
-canny=cv.Canny(src,100,200)
-contours,hierarchy=cv.findContours(canny,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+src = cv.imread("E:/practice/openCV/material/images/stuff.jpg")
+canny = cv.Canny(src, 80, 160)
+k=cv.getStructuringElement(cv.MORPH_RECT,(3,3)) # 膨胀需要的
+dilate=cv.dilate(canny,k)
+contours,hierarchy=cv.findContours(dilate,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
 for i in range(len(contours)):
-    cv.drawContours(src,contours, i,(0,0,255))
-
+    rect=cv.minAreaRect(contours[i])
+    box=cv.boxPoints(rect)
+    box=np.int0(box)
+    cv.drawContours(src,[box],0,(0,0,255),1)
 cv.imshow("dst",src)
 
 cv.waitKey(0)
