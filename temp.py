33d1ef39b72c4228f2219dcc7493d5cfdc9e6de8
赵睿 <zrendless@foxmail.com>
2020-08-18 00:02:28 +0800

    contourArea面积求取,arcLength弧长计算

--- temp.py
+++ temp.py
@@ -1,16 +1,23 @@
 ﻿import cv2.cv2 as cv
 import numpy as np
-
-src = cv.imread("E:/practice/openCV/material/images/stuff.jpg")
-canny = cv.Canny(src, 80, 160)
-k=cv.getStructuringElement(cv.MORPH_RECT,(3,3)) # 膨胀需要的
+src= cv.imread("E:/practice/openCV/material/images/zhifang_ball.png")
+cv.imshow("src",src)
+gauss= cv.GaussianBlur(src,(3,3),0)
+canny=cv.Canny(src,100,200)
+k=cv.getStructuringElement(cv.MORPH_RECT,(3,3))
 dilate=cv.dilate(canny,k)
 contours,hierarchy=cv.findContours(dilate,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
 for i in range(len(contours)):
+    theArea=cv.contourArea(contours[i])
+    theArcLength=cv.arcLength(contours[i],True)
+    if theArea<100 or theArcLength<100:
+        continue
     rect=cv.minAreaRect(contours[i])
+    x,y= rect[0]
     box=cv.boxPoints(rect)
     box=np.int0(box)
-    cv.drawContours(src,[box],0,(0,0,255),1)
+    cv.drawContours(src,[box],0,(0,0,255))
+    cv.circle(src,(np.int32(x),np.int32(y)),2,(255,0,0),-1)
 cv.imshow("dst",src)
 
 cv.waitKey(0)
