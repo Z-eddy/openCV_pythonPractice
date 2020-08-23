c53ee9a2ea5f2865264c1c34affd35443a18b88d
赵睿 <zrendless@foxmail.com>
2020-08-23 22:25:02 +0800

    approxPolyDP轮廓逼近

--- temp.py
+++ temp.py
@@ -1,23 +1,24 @@
 ﻿import cv2.cv2 as cv
 import numpy as np
-src= cv.imread("E:/practice/openCV/material/images/zhifang_ball.png")
-cv.imshow("src",src)
-gauss= cv.GaussianBlur(src,(3,3),0)
-canny=cv.Canny(src,100,200)
-k=cv.getStructuringElement(cv.MORPH_RECT,(3,3))
-dilate=cv.dilate(canny,k)
-contours,hierarchy=cv.findContours(dilate,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
+src = cv.imread("E:/practice/openCV/material/images/contours.png")
+canny = cv.Canny(src, 80, 160)
+contours,hierarchy= cv.findContours(canny,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
 for i in range(len(contours)):
-    theArea=cv.contourArea(contours[i])
-    theArcLength=cv.arcLength(contours[i],True)
-    if theArea<100 or theArcLength<100:
-        continue
-    rect=cv.minAreaRect(contours[i])
-    x,y= rect[0]
-    box=cv.boxPoints(rect)
-    box=np.int0(box)
-    cv.drawContours(src,[box],0,(0,0,255))
-    cv.circle(src,(np.int32(x),np.int32(y)),2,(255,0,0),-1)
+    rotateRect = cv.minAreaRect(contours[i])
+    x,y= rotateRect[0]
+    x=np.int32(x)
+    y= np.int32(y)
+    curve = cv.approxPolyDP(contours[i], 4, True)
+    vertex = curve.shape[0]
+    if vertex == 3:
+        cv.putText(src,"Triangle",(x,y),cv.FONT_ITALIC,0.8,(0,0,255))
+    elif vertex == 4:
+        cv.putText(src,"Rectangle",(x,y),cv.FONT_ITALIC,0.8,(0,0,255))
+    elif vertex == 6:
+        cv.putText(src,"Polygon",(x,y),cv.FONT_ITALIC,0.8,(0,0,255))
+    elif vertex >= 10:
+        cv.putText(src,"Cycle",(x,y),cv.FONT_ITALIC,0.8,(0,0,255))
+
 cv.imshow("dst",src)
 
 cv.waitKey(0)
